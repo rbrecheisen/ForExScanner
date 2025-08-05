9941570f167a2ad4ed9748eb5c0ab472393194e2
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-08-05 16:45:30 +0200

    Saving work

---
+++ forexscanner/src/forexscanner/constants.py
@@ -0,0 +1,4 @@
+FX_ACCOUNT_ID = '101-004-30581840-001'
+FX_API_KEY = 'a856b96f70cfe8fc507765857fa98b56-94e46be1ae266b09fa59fb259ec9dcd5'
+FX_API_URL = 'https://api-fxpractice.oanda.com' # https://api-fxtrade.oanda.com (live)
+FX_ENVIRONMENT = 'practice' # "live" for live environment
--- forexscanner/src/forexscanner/main.py
+++ forexscanner/src/forexscanner/main.py
@@ -2,13 +2,12 @@ import oandapyV20
 
 from oandapyV20.endpoints.accounts import AccountDetails
 
-API_KEY = 'a856b96f70cfe8fc507765857fa98b56-94e46be1ae266b09fa59fb259ec9dcd5'
-ACCOUNT_ID = '101-004-30581840-001'
-API_URL = "https://api-fxpractice.oanda.com"  # Use 'https://api-fxtrade.oanda.com' for LIVE accounts
+import forexscanner.constants as constants
 
-client = oandapyV20.API(access_token=API_KEY, environment="practice")  # use 'live' for live trading
+client = oandapyV20.API(
+    access_token=constants.FX_API_KEY, environment=constants.FX_ENVIRONMENT)
 
-request = AccountDetails(accountID=ACCOUNT_ID)
+request = AccountDetails(accountID=constants.FX_ACCOUNT_ID)
 response = client.request(request)
 
 print(response)
